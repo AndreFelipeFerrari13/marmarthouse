27da15ff6ac52d5d2f292012769fa3a7fdbf65bd
André Ferrari <141585899+AndreFelipeFerrari13@users.noreply.github.com>
2024-12-07 00:55:33 -0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ export function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-20">
           <div className="flex items-center">
-            <img src="/logo.png" alt="Art House Marmoraria" className="h-12" />
+            <img src="/logo.jpg" alt="Art House Marmoraria" className="h-12" />
           </div>
           
           <div className="hidden md:flex items-center space-x-8">
